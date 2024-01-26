426a53933fd286ecf4f1c30e5607173b30644294
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-01-26 13:44:15 +0200

    added footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/Hero";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 };
