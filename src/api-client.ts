502b82d5b30dd1e88837e803cc894664df93a19f
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-01-28 11:57:23 +0200

    implemented login functionality

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,4 +1,4 @@
-import { RegisterFormData } from "@/common/types/auth";
+import { RegisterFormData, SignInFormData } from "@/common/types/auth";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -18,6 +18,23 @@ export const register = async (formData: RegisterFormData) => {
   if (!response.ok) {
     throw new Error(responseBody.message);
   }
+};
+
+export const signIn = async (formData: SignInFormData) => {
+  const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const body = await response.json();
+  if (!response.ok) {
+    throw new Error(body.message);
+  }
+  return body;
 };
 
 export const validateToken = async () => {
