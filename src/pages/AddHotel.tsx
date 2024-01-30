210d5966fad2fb18c3561cc530d5c84dfb12770b
Mr-Vitalii <co4ckowoi.v@gmail.com>
2024-01-30 17:58:48 +0200

    created a form to add a hotel

---
+++ src/pages/AddHotel.tsx
@@ -0,0 +1,6 @@
+import { ManageHotelForm } from "@/components/forms/ManageHotelForm/ManageHotelForm";
+import { useMutation } from "react-query";
+
+export const AddHotel = () => {
+  return <ManageHotelForm />;
+};
